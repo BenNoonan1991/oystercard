1f4ab1a4ff5c590ace78f721afb4abaccb8926e7
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-20 14:38:13 +0100

    Refactors tests using context block

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,7 +15,32 @@ describe Oystercard do
     before { oystercard.top_up(Oystercard::FARE) }
     before { oystercard.touch_in(entry_station) }
 
-    it 'stores a journey' do
+    describe '#touch_out' do
+      it "deducts fare amount on touch out" do
+        expect { oystercard.touch_out(exit_station) }.to change{oystercard.balance}.by(-Oystercard::FARE)
+      end
+    end
+
+    describe '#in_journey do' do
+      it "it changes #in_journey to true" do
+        expect(oystercard).to be_in_journey
+      end
+
+    it "it changes #in_journey to false" do
+      oystercard.touch_out(exit_station)
+      expect(oystercard.in_journey?).to eq false
+    end
+  end
+
+  describe '@journeys' do
+    it 'check that journey is created after touching in/out' do
+      oystercard.touch_out(exit_station)
+      expect(oystercard.journeys).not_to be_empty
+    end
+  end
+
+  describe "#journey" do
+      it 'stores a journey' do
       oystercard.touch_out(exit_station)
       expect(oystercard.journeys).to include journey
     end
@@ -23,6 +48,7 @@ describe Oystercard do
       expect(oystercard.entry_station).to eq [entry_station]
     end
   end
+end 
 
     describe "#balance" do
       it "should return 0 balance" do
@@ -41,36 +67,8 @@ describe Oystercard do
     end
 
     describe '#touch_in' do
-      it "it changes #in_journey to true" do
-        oystercard.top_up(Oystercard::FARE)
-        oystercard.touch_in(entry_station)
-        expect(oystercard).to be_in_journey
-      end
       it "raises error if funds below £1" do
       expect{oystercard.touch_in(entry_station)}.to raise_error "Funds too low"
     end
   end
-
-    describe '#touch_out' do
-      it "it changes #in_journey to false" do
-        oystercard.top_up(10)
-        oystercard.touch_in(entry_station)
-        oystercard.touch_out(exit_station)
-        expect(oystercard.in_journey?).to eq false
-     end
-      it "deducts fare amount on touch out" do
-        oystercard.top_up(10)
-        oystercard.touch_in(entry_station)
-        expect { oystercard.touch_out(exit_station) }.to change{oystercard.balance}.by(-Oystercard::FARE)
-      end
-    end
-
-    describe '@journeys' do
-      it 'check that journey is created after touching in/out' do
-        oystercard.top_up(10)
-        oystercard.touch_in(entry_station)
-        oystercard.touch_out(exit_station)
-        expect(oystercard.journeys).not_to be_empty
-      end
-    end
 end
