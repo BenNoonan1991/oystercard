e5030a932eab127cc6873fe1e71afce970d4e5c7
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-20 16:37:24 +0100

    Updated journey spec

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,12 @@
 class Journey
+
+  PENALTY_FARE = 6
+
+  attr_reader :entry_station, :exit_station
+
+  def initialize(entry_station = PENALTY_FARE)
+    @entry_station = entry_station
+    @exit_station = exit_station
+  end
+
 end
