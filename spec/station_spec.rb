6422a0ad4f5c433a311fe067461802cfb065e142
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-20 15:18:41 +0100

    Adds name and zone variables to station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,7 +3,15 @@ require './lib/station'
 describe Station do
   subject(:station) { described_class.new }
 
-  it 'creates a new instance of Station' do
-    expect(station).to eq station
+    it "checks zone variable is responding" do
+      expect(:zone).not_to be_nil
+    end
+
+    it "checks name variable is repsonding" do
+      expect(:name).not_to be_nil
+    end
+
+    it 'creates a new instance of Station' do
+      expect(station).to eq station
+    end
   end
-end
